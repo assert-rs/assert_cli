7a81c5ff0ebc4afbe7e75e219aad3f470f6e1b11
Ed Page <eopage@gmail.com>
2018-05-25 14:01:29 +0200

    test: Assert::example

--- tests/cargo.rs
+++ tests/cargo.rs
@@ -13,7 +13,19 @@ fn main_binary() {
 
 #[test]
 fn cargo_binary() {
-    assert_cli::Assert::cargo_binary("assert_fixture")
+    assert_cli::Assert::cargo_binary("bin_fixture")
+        .with_env(assert_cli::Environment::inherit().insert("stdout", "42"))
+        .stdout()
+        .is("42")
+        .stderr()
+        .is("")
+        .unwrap();
+}
+
+#[test]
+fn cargo_example() {
+    assert_cli::Assert::example("example_fixture")
+        .unwrap()
         .with_env(assert_cli::Environment::inherit().insert("stdout", "42"))
         .stdout()
         .is("42")
